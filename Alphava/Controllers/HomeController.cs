9fd844ad153511dbd6df8c8ec393b2c8db332380
nilaykaratop <nilay.karatop@gmail.com>
2020-06-28 18:51:46 +0300

    WEP API

---
+++ Alphava/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Alphava.Models;
+using RestSharp;
+using Newtonsoft.Json;
+
+namespace Alphava.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            //base
+            var client = new RestClient("https://www.alphavantage.co");
+            //resource
+            var request = new RestRequest("/query?function=TIME_SERIES_WEEKLY&symbol=IBM&apikey=3LB8NNIVP20U94NH");
+            //client üzerinden requesti servise yolluyoruz
+            var content = client.Execute(request).Content;
+            // json'daki verileri kendi uygulamamızdaki Class’a aktarmak için
+            StockData quertresult = JsonConvert.DeserializeObject<StockData>(content);
+            return View(quertresult);
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Alphava/Models/AlphaData.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+
+namespace Alphava.Models
+{
+
+    public class StockData
+    {
+        [JsonProperty("Meta Data")]
+        public MetaData MetaData { get; set; }
+
+        [JsonProperty("Weekly Time Series")]
+        public Dictionary<string, WeeklyTimeSery> WeeklyTimeSeries { get; set; }
+    }
+
+    public class MetaData
+    {
+        [JsonProperty("1. Information")]
+        public string The1Information { get; set; }
+
+        [JsonProperty("2. Symbol")]
+        public string The2Symbol { get; set; }
+
+        [JsonProperty("3. Last Refreshed")]
+        public DateTimeOffset The3LastRefreshed { get; set; }
+
+        [JsonProperty("4. Time Zone")]
+        public string The4TimeZone { get; set; }
+    }
+
+    public class WeeklyTimeSery
+    {
+        [JsonProperty("1. open")]
+        public string The1Open { get; set; }
+
+        [JsonProperty("2. high")]
+        public string The2High { get; set; }
+
+        [JsonProperty("3. low")]
+        public string The3Low { get; set; }
+
+        [JsonProperty("4. close")]
+        public string The4Close { get; set; }
+
+        [JsonProperty("5. volume")]
+        public long The5Volume { get; set; }
+    }
+}
+
+
+    
+
